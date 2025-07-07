17ea3e0f4ef73dc01d19f271d97f6683f9ef350e
sheeezh12 <awikwokgit71@gmail.com>
2025-07-07 17:04:41 +0700

    pre feature

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from flask import Flask, render_template
 
 def write(origin, shorten):
     with open("db/db.json", "r") as file:
@@ -13,7 +14,15 @@ def write(origin, shorten):
             json.dump(data, file, indent=4)    
             print("UDAH REK")      
 
-    
+app = Flask(
+    __name__,
+    template_folder=os.path.join(os.path.dirname(__file__), '..', 'template'),
+    static_folder=os.path.join(os.path.dirname(__file__), '..', 'static')
+)
 
+@app.route("/")
+def main():
+    return render_template("index.html")
 
-write("internetcafe.com", "anakmodnyet")  
+if __name__ == "__main__":
+    app.run(debug=True)
